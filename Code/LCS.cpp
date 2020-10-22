abdf84f4e6c9a8a0b265b00a4428fde5e8b4c800
DESKTOP-ETVR2PH\User <leo965380654@gmail.com>
2020-10-23 00:59:09 +0800

    Longest common subsequence

--- Code/LCS.cpp
+++ Code/LCS.cpp
@@ -6,14 +6,14 @@
 using namespace std;
 
 int LCS(string s1, string s2) {
-	int m = s1.size() + 1;
-	int n = s2.size() + 1;
-	vector< vector<int> > LCS_matrix(m, vector<int>(n));
+	int m = s1.size();
+	int n = s2.size();
+	vector< vector<int> > LCS_matrix(m+1, vector<int>(n+1));
 
 	if (s1.empty() || s2.empty()) { return 0; }
 	else {
-		for (int i = 1; i < m; i++) {
-			for (int j = 1; j < n; j++) {
+		for (int i = 1; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
 				//cout << s1.at(i-1) << "," << s2.at(j-1) << endl;
 
 				if (s1.at(i-1) == s2.at(j-1)) {
@@ -24,7 +24,7 @@ int LCS(string s1, string s2) {
 				}
 			}
 		}
-		return LCS_matrix[m-1][n-1];
+		return LCS_matrix[m][n];
 	}
 }
 
