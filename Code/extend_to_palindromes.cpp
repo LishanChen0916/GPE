a3822f657facf943485f0110e6d70a0e89c1ecfd
DESKTOP-ETVR2PH\User <leo965380654@gmail.com>
2020-10-24 01:26:47 +0800

    Extend to Palindromes

---
+++ Code/extend_to_palindromes.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+	while (true) {
+		string s;
+		cin >> s;
+
+		if (!cin) { break; }
+
+		int matched = 0;
+		for (auto it = s.begin(); it != s.end(); it++) {
+			for (auto r_it = s.rbegin(); r_it != s.rend(); r_it++) {
+				if (*it == *r_it) { 
+					matched += 1;
+					if (it + 1 != s.end()) { it++; }
+					else { break; }
+				}
+				else { 
+					matched = 0;
+					break; 
+				}
+			}
+		}
+		cout << matched << endl;
+
+		cout << s;
+		for (auto r_it = s.rbegin() + matched; r_it != s.rend(); r_it++) {
+			cout << *r_it;
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
